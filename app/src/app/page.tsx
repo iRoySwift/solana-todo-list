7ee03162148f5fd243eefdeb15f48e1e06840fd8
Roy <iRoySwift@iCloud.com>
2023-11-16 10:20:43 +0800

    page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,11 +5,16 @@ import React from "react";
 import TodoList from "@/views/Home/TodoList";
 import Loading from "@/components/Loading";
 import { useTodo } from "@/hooks/todo";
+import Wallets from "@/components/Wallets";
+import { Box } from "@mui/material";
 
 export default function Home() {
     const { loading } = useTodo();
     return (
         <main className={styles.main}>
+            <Box sx={{ position: "fixed", top: 10, right: 10 }}>
+                <Wallets />
+            </Box>
             <Loading loading={loading}>
                 <TodoList />
             </Loading>
