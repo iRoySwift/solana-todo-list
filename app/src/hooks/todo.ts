8155a68d2ec3d6b2b35b0f18a09747cf505d516e
Roy <iRoySwift@iCloud.com>
2023-11-24 15:33:59 +0800

    todo

--- app/src/hooks/todo.ts
+++ app/src/hooks/todo.ts
@@ -2,11 +2,27 @@
 import React, {
     ChangeEvent,
     KeyboardEvent,
+    useCallback,
     useEffect,
     useMemo,
     useRef,
     useState,
 } from "react";
+import {
+    useAnchorWallet,
+    useConnection,
+    useWallet,
+} from "@solana/wallet-adapter-react";
+import { SystemProgram } from "@solana/web3.js";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { utf8 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import toast from "react-hot-toast";
+// import todoIdl from "../../../target/idl/solana_todo_list.json";
+import todoIdl from "../constants/idl.json";
+import { TODO_PROGRAM_PUBKEY } from "@/constants";
+import { authorFilter } from "@/utils/authorFilter";
+
 export type iState = "all" | "active" | "completed";
 
 type iTodo = {
@@ -46,17 +62,69 @@ export const useTodo = () => {
     const [selected, setSelected] = useState(false);
     const [todoList, setTodoList] = useState<iTodo[]>([]);
     const [state, setState] = useState<iState>("all");
-    const [initialized, setInitialized] = useState(false);
     const [loading, setLoading] = useState(false);
+    const [initialized, setInitialized] = useState(false);
+    const [transactionPending, setTransactionPending] = useState(false);
+    const [lastTodo, setLastTodo] = useState(0);
+
+    const { connection } = useConnection();
+    const { publicKey } = useWallet();
+    const anchorWallet = useAnchorWallet();
+
+    const program = useMemo(() => {
+        // Configure the client to use the local cluster.
+        // const provider = anchor.AnchorProvider.env();
+        // anchor.setProvider(provider);
+        // const payer = provider.wallet as anchor.Wallet;
+        // const program = anchor.workspace.SolanaTodoList as Program<SolanaTodoList>;
+        if (anchorWallet) {
+            const provider = new anchor.AnchorProvider(
+                connection,
+                anchorWallet,
+                anchor.AnchorProvider.defaultOptions()
+            );
+            return new anchor.Program(
+                todoIdl as any,
+                TODO_PROGRAM_PUBKEY,
+                provider
+            );
+        }
+    }, [connection, anchorWallet]);
+
+    /** get use pda */
+    const getUserPda = useCallback(() => {
+        if (!(publicKey && program)) return;
+        const [userPda, userBump] =
+            anchor.web3.PublicKey.findProgramAddressSync(
+                [utf8.encode("USER_ACCOUNT1"), publicKey.toBuffer()],
+                program?.programId
+            );
+        return userPda;
+    }, [publicKey, program]);
+
+    /** get todo pda */
+    const getTodoPda = useCallback(
+        (lastTodo: number) => {
+            if (!(publicKey && program)) return;
+            const [todoPda, todoBump] =
+                anchor.web3.PublicKey.findProgramAddressSync(
+                    [
+                        utf8.encode("TODO_ACCOUNT1"),
+                        publicKey.toBuffer(),
+                        Buffer.from([lastTodo]),
+                    ],
+                    program?.programId
+                );
+            return todoPda;
+        },
+        [publicKey, program]
+    );
 
     const handleKeyChange = (e: ChangeEvent<HTMLInputElement>) => {
         setInput(e.target.value);
     };
-    const initializedStaticUser = () => {
-        setInitialized(true);
-    };
 
-    const addTodo = (e: KeyboardEvent<HTMLInputElement>) => {
+    const addStaticTodo = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key !== "Enter" || !input.trim().length) return;
         const idx =
             (todoList.length > 0
@@ -75,7 +143,36 @@ export const useTodo = () => {
         setInput("");
     };
 
-    const markTodo = (idx: number, checked: boolean) => {
+    const addTodo = async (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key !== "Enter" || !input.trim().length) return;
+        if (!(publicKey && program)) return;
+        let userPda = getUserPda();
+        let todoPda = getTodoPda(lastTodo);
+        if (!userPda || !todoPda) return;
+        setTransactionPending(true);
+        const ts = await program.methods
+            .addTodo(input)
+            .accounts({
+                todoAccount: todoPda,
+                userAccount: userPda,
+                authority: publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .rpc()
+            .then(tx => {
+                toast.success("Successfully added todo!");
+                setTransactionPending(false);
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.toString());
+            })
+            .finally(() => {
+                setInput("");
+            });
+    };
+
+    const markStaticTodo = (idx: number, checked: boolean) => {
         setTodoList(preTodoList =>
             preTodoList.map(todo => {
                 if (todo.account.idx === idx) {
@@ -86,7 +183,35 @@ export const useTodo = () => {
         );
     };
 
-    const updateTodoContent = (idx: number, content: string) => {
+    const markTodo = (idx: number, checked: boolean) => {
+        if (!(publicKey && program)) return;
+        let userPda = getUserPda();
+        let todoPda = getTodoPda(idx);
+        if (!userPda || !todoPda) return;
+        setTransactionPending(true);
+        program.methods
+            .markTodo(idx)
+            .accounts({
+                todoAccount: todoPda,
+                userAccount: userPda,
+                authority: publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .rpc()
+            .then(tx => {
+                toast.success("Successfully mark todo!");
+                setTransactionPending(false);
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.toString());
+            });
+        // .finally(() => {
+        //     setInput("");
+        // });
+    };
+
+    const updateStaticTodo = (idx: number, content: string) => {
         setTodoList(preTodoList =>
             preTodoList.map(todo => {
                 if (todo.account.idx === idx) {
@@ -97,13 +222,42 @@ export const useTodo = () => {
         );
     };
 
-    const deleteTodo = (idx: number) => {
+    const removeStaticTodo = (idx: number) => {
         setTodoList(preTodoList =>
             preTodoList.filter(todo => todo.account.idx !== idx)
         );
     };
 
-    const toggleAll = () => {
+    const removeTodo = (idx: number) => {
+        if (!(publicKey && program)) return;
+        let userPda = getUserPda();
+        let todoPda = getTodoPda(idx);
+        if (!userPda || !todoPda) return;
+        setTransactionPending(true);
+        program.methods
+            .removeTodo(idx)
+            .accounts({
+                todoAccount: todoPda,
+                userAccount: userPda,
+                authority: publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .rpc()
+            .then(tx => {
+                console.log("🚀 ~ file: todo.ts:247 ~ removeTodo ~ tx:", tx);
+                toast.success("Successfully mark todo!");
+                setTransactionPending(false);
+            })
+            .catch(error => {
+                console.log(error);
+                toast.error(error.toString());
+            });
+        // .finally(() => {
+        //     setInput("");
+        // });
+    };
+
+    const toggleStaticAll = () => {
         setSelected(pre => !selected);
     };
 
@@ -111,15 +265,78 @@ export const useTodo = () => {
         setState(v);
     };
 
-    const clearAllCompleted = () => {
+    const clearStaticCompleted = () => {
         setTodoList(preTodoList =>
             preTodoList.filter(todo => !todo.account.marked)
         );
     };
 
+    /** initialize User PDA*/
+    const initializeUser = useCallback(async () => {
+        if (!(publicKey && program) && initialized) return;
+        try {
+            setTransactionPending(true);
+            let userPda = getUserPda();
+            if (!userPda) return;
+            const tx = await program?.methods
+                .initializeUser()
+                .accounts({
+                    userAccount: userPda,
+                    authority: publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .rpc();
+            toast.success("Successfully initialized user.");
+            setTransactionPending(false);
+        } catch (error) {
+            console.log(error);
+            toast.error(error!.toString());
+        }
+    }, [getUserPda, initialized, program, publicKey]);
+
+    /** 获取todo数据 */
+    const getTodoAccount = useCallback(async () => {
+        if (!(publicKey && program)) return;
+        const todoAccount: any = await program?.account.todoAccount.all([
+            authorFilter(publicKey!.toString()) as any,
+        ]);
+        setTodoList(todoAccount);
+    }, [publicKey, program]);
+
+    /** 初始化 */
+    const init = useCallback(async () => {
+        if (!(publicKey && program && !transactionPending)) return;
+        setLoading(true);
+        let userPda = getUserPda();
+        if (!userPda) return;
+        program?.account.userAccount
+            .fetch(await userPda)
+            .then((res: any) => {
+                setLastTodo(res.lastTodo);
+                getTodoAccount();
+            })
+            .catch(() => {
+                initializeUser();
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [
+        getTodoAccount,
+        getUserPda,
+        initializeUser,
+        program,
+        publicKey,
+        transactionPending,
+    ]);
+
     useEffect(() => {
-        if (initialized) setTodoList(staticTodos);
-    }, [initialized]);
+        init();
+    }, [init]);
+
+    // useEffect(() => {
+    //     initializeUser();
+    // }, [initializeUser]);
 
     useEffect(() => {
         setTodoList(preTodoList =>
@@ -162,6 +379,7 @@ export const useTodo = () => {
     );
 
     return {
+        init,
         input,
         // setInput,
         // selected,
@@ -173,12 +391,15 @@ export const useTodo = () => {
         loading,
         handleKeyChange,
         addTodo,
+        addStaticTodo,
         markTodo,
-        updateTodoContent,
-        deleteTodo,
-        toggleAll,
+        markStaticTodo,
+        updateStaticTodo,
+        removeTodo,
+        removeStaticTodo,
+        toggleStaticAll,
+        clearStaticCompleted,
         handleStateChange,
-        clearAllCompleted,
         activeTodo,
         completeTodo,
         leftItem,
