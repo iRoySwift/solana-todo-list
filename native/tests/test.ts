0ff37989eadd9da061bfbb72ff57780705e17bbc
Roy <iRoySwift@iCloud.com>
2023-12-01 15:38:40 +0800

    test add todo

---
+++ native/tests/test.ts
@@ -0,0 +1,65 @@
+import {
+    Connection,
+    PublicKey,
+    Transaction,
+    TransactionInstruction,
+    clusterApiUrl,
+    sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { describe, it } from "node:test";
+import { addTodo, createKeypairFromFile, initializeUser } from "../ts";
+
+// Step 1 连接到Solana网络 devnet
+const devnet = clusterApiUrl("devnet");
+const connection = new Connection(process.env.DEVNET || devnet, "confirmed");
+
+// Step 2 创建者账号信息（private key）
+// const signer = Keypair.fromSecretKey(new Uint8Array(secretKeyArray));
+const payer = createKeypairFromFile(
+    require("os").homedir() + "/.config/solana/id.json"
+);
+
+const program = createKeypairFromFile(
+    "./../target/deploy/native_dapp_todo-keypair.json"
+);
+
+function deriveUserPda() {
+    return PublicKey.findProgramAddressSync(
+        [Buffer.from("USER_ACCOUNT"), payer.publicKey.toBuffer()],
+        program.publicKey
+    );
+}
+
+function deriveTodoPda() {
+    return PublicKey.findProgramAddressSync(
+        [
+            Buffer.from("TODO_ACCOUNT"),
+            payer.publicKey.toBuffer(),
+            Buffer.from([0]),
+        ],
+        program.publicKey
+    );
+}
+
+describe("todo list", async () => {
+    it("init user", async () => {
+        const [userPda, bump] = deriveUserPda();
+        const ix = initializeUser(userPda, payer.publicKey, program.publicKey);
+        const tx = await sendAndConfirmTransaction(
+            connection,
+            new Transaction().add(ix),
+            [payer]
+        );
+        console.log("🚀 ~ file: test.ts:42 ~ it ~ tx:", tx);
+    });
+    it("add todo", async () => {
+        const [todoPda, bump] = deriveTodoPda();
+        const ix = addTodo(todoPda, payer.publicKey, program.publicKey, "test");
+        const tx = await sendAndConfirmTransaction(
+            connection,
+            new Transaction().add(ix),
+            [payer]
+        );
+        console.log("🚀 ~ file: test.ts:63 ~ it ~ tx:", tx);
+    });
+});
