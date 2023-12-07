8e3ccf1fc4206b36a1edb0c9e304a36613a7984e
Roy <iRoySwift@iCloud.com>
2023-12-07 09:28:35 +0800

    init user

--- native/program/src/instructions/initialize_user.rs
+++ native/program/src/instructions/initialize_user.rs
@@ -2,26 +2,30 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
+    // program_error::ProgramError,
     pubkey::Pubkey,
 };
 
 use crate::{error::TodoError, states::UserAccount};
 
-pub fn initialize_user(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    user_account: UserAccount,
-) -> ProgramResult {
+pub fn initialize_user(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let accounts_iters = &mut accounts.iter();
-    let target_account = next_account_info(accounts_iters)?;
+    let target = next_account_info(accounts_iters)?;
     let payer = next_account_info(accounts_iters)?;
-    let (pda, bump) =
+    let system_program = next_account_info(accounts_iters)?;
+    let (pda, _bump) =
         Pubkey::find_program_address(&[UserAccount::SEED_PREFIX, payer.key.as_ref()], program_id);
-    msg!("target:{:?}", target_account);
-    msg!("user_account:{:?}", user_account);
+    msg!("program_id:{:?}", program_id);
+    msg!("target:{:?}", target);
+    msg!("payer:{:?}", payer);
+    msg!("pda:{:?}", pda);
+    msg!("system_program:{:?}", system_program);
 
-    if target_account.key != &pda {
+    if target.key != &pda {
         return Err(TodoError::NotOwnedByAccount.into());
     }
+    // if target.owner != program_id {
+    //     return Err(ProgramError::InvalidAccountOwner);
+    // }
     Ok(())
 }
