cf9f9d70b06524a40ce2d2cef001e61440784f35
Roy <iRoySwift@iCloud.com>
2023-12-19 17:45:23 +0800

    init user

--- native/program/src/instructions/initialize_user.rs
+++ native/program/src/instructions/initialize_user.rs
@@ -1,9 +1,13 @@
+use borsh::BorshSerialize;
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
-    // program_error::ProgramError,
+    program::invoke_signed,
     pubkey::Pubkey,
+    rent::Rent,
+    system_instruction,
+    sysvar::Sysvar,
 };
 
 use crate::{error::TodoError, states::UserAccount};
@@ -12,20 +16,43 @@ pub fn initialize_user(program_id: &Pubkey, accounts: &[AccountInfo]) -> Program
     let accounts_iters = &mut accounts.iter();
     let target = next_account_info(accounts_iters)?;
     let payer = next_account_info(accounts_iters)?;
-    let system_program = next_account_info(accounts_iters)?;
-    let (pda, _bump) =
+
+    let (pda, bump) =
         Pubkey::find_program_address(&[UserAccount::SEED_PREFIX, payer.key.as_ref()], program_id);
-    msg!("program_id:{:?}", program_id);
-    msg!("target:{:?}", target);
-    msg!("payer:{:?}", payer);
-    msg!("pda:{:?}", pda);
-    msg!("system_program:{:?}", system_program);
 
     if target.key != &pda {
         return Err(TodoError::NotOwnedByAccount.into());
     }
-    // if target.owner != program_id {
-    //     return Err(ProgramError::InvalidAccountOwner);
-    // }
+
+    // close target account
+    // msg!("rent{:?}", target.lamports());
+    // **payer.lamports.borrow_mut() += target.lamports();
+    // **target.try_borrow_mut_lamports()? = 0;
+
+    let user_data = UserAccount {
+        authority: payer.key.to_string(),
+        last_todo: 0,
+        todo_total: 0,
+    };
+
+    let space = user_data.try_to_vec()?.len() as usize;
+    let lamports = (Rent::get()?).minimum_balance(space);
+    let instruction = &system_instruction::create_account(
+        payer.key,
+        target.key,
+        lamports,
+        space as u64,
+        program_id,
+    );
+    let account_infos = &[target.clone(), payer.clone()];
+    let signers_seeds: &[&[&[u8]]] = &[&[UserAccount::SEED_PREFIX, payer.key.as_ref(), &[bump]]];
+
+    invoke_signed(instruction, account_infos, signers_seeds)?;
+
+    // user_data.serialize(&mut &mut target.data.borrow_mut()[..])?;
+    user_data.serialize(&mut *target.data.borrow_mut())?;
+    msg!("user_data{:?}", user_data);
+    msg!("target:{:?}", target);
+    msg!("target data:{:?}", target.data);
     Ok(())
 }
